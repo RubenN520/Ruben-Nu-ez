3ee10a14711c7357817d7d4016d73fabcc3912ac
Ruben <sanabriaruben844@gmail.com>
2023-10-30 18:49:59 -0500

    Segunda modificacion

--- prueba.py
+++ prueba.py
@@ -1,1 +1,8 @@
 print("Hola mundo")
+
+print("Vamos hacer una suma")
+
+num1 = int(input("Digite el primer número"))
+num2 = int(input("Digite el primer número"))
+operacion = num1 + num2
+print("El resultado es: ",operacion)
