1c088ae91f73dbba8c8e4882b4d482e63e658b93
Ruben <sanabriaruben844@gmail.com>
2023-10-30 18:58:32 -0500

    Tercera modificacion

--- prueba.py
+++ prueba.py
@@ -6,3 +6,5 @@ num1 = int(input("Digite el primer número"))
 num2 = int(input("Digite el primer número"))
 operacion = num1 + num2
 print("El resultado es: ",operacion)
+
+print("prueba para pasar a rama ")
